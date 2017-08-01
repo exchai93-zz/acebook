c5e5d8577b53f647be03d8d51d4c89ca620ab4d8
Emily Chai <echai93@gmail.com>
2017-08-01 15:39:51 +0100

    Add comments route - nested resource within Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'homepage/index'
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   root 'homepage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
