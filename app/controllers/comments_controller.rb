9625361b769a9a9ac2deb764a220a9b38f606a86
Emily Chai <echai93@gmail.com>
2017-08-03 21:55:13 +0100

    Ability to delete commentS

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,13 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
   private
 
   def comment_params
