b66002d7f3ad0a2467a43ff8f573a46980a71b68
Emily Chai <echai93@gmail.com>
2017-08-03 21:36:10 +0100

    Change post_id to a symbol

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
-    @post = Post.find(params[post_id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
   end
@@ -11,5 +11,5 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:commenter, :body)
   end
-  
+
 end
