b2b7856b14dc40d131997ae4dd3db81a3641757f
Emily Chai <echai93@gmail.com>
2017-08-03 21:27:22 +0100

    Move methods to controller

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,15 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :post
 
-  def create
-    @post = Post.find(params[post_id])
-    @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post)
-  end
-
-  private
-
-  def comment_params
-    params.require(:comment).permit(:commenter, :body)
-  end
 end
