3b46880fdc7281210e8d40e36a9af8eb4247a5c8
Emily Chai <echai93@gmail.com>
2017-08-09 15:14:29 +0100

    Add routing for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get 'users/new'
-
   get 'homepage/index'
 
   resources :posts do
     resources :comments
   end
+
+  resources :users
 
   root 'homepage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
