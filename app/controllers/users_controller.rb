c8653d3c7021ab8a060363fef28f2d1e0c394e9c
Emily Chai <echai93@gmail.com>
2017-08-10 08:49:24 +0100

    Create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
 
+  def index
+  end
+  
   def show
     @user = User.find(params[:id])
-    redirect_to :controller => "main", :action => "index"
   end
 
   def new
-    @user = User.new 
+    @user = User.new
   end
 
 end
